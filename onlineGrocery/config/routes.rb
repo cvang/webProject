b503cbbaa448de11a3312255aebfc90ae0c22d0d
cvang <cvang32@mail.csuchico.edu>
2015-12-09 11:55:12 -0800

    adds show to devise

--- onlineGrocery/config/routes.rb
+++ onlineGrocery/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  #match '/users/:id', :to => "users#show", :as => :user_show, :via => :get
   resources :images
   devise_for :users
-  resources :users, :only => [:edit, :update]
+  resources :users, :only => [:show, :edit, :update]
   resources :products
     root "products#index"
 end
