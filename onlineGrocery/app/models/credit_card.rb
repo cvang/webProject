98fb5cee5027301eb5ac2b19e753c56f9b7e4813
cvang <cvang32@mail.csuchico.edu>
2015-12-12 01:41:30 -0800

    adds css to files

--- onlineGrocery/app/models/credit_card.rb
+++ onlineGrocery/app/models/credit_card.rb
@@ -1,3 +1,6 @@
 class CreditCard < ActiveRecord::Base
+    validates :card_numbers, presence: true
+    validates :expire, presence: true
+    validates :cvs, presence: true
   belongs_to :user
 end
--- onlineGrocery/config/routes.rb
+++ onlineGrocery/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :checkout, :only =>[:show, :edit, :update, :create, :index]
   resources :images
   devise_for :users
   resources :users, :only => [:show, :edit, :update, :create] do 
